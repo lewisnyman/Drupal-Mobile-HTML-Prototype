b0a011c3593fdab60ba9cd96db82ea521758ac3a
Lewis Nyman <hello@lewisnyman.co.uk>
2012-02-11 17:31:21 +0000

    Issue #1430384 by lewisnyman: Integrated swipe.js for main navigation at /admin. One to one swiping!

--- _assets/js/script.js
+++ _assets/js/script.js
@@ -2,6 +2,11 @@ $(document).ready(function(){ ready(); }); // call function after DOM is ready t
 
 function ready() {
 var adminlinks = $('.admin-list .leaf a');
+
+window.slider = new Swipe(
+  document.getElementById('wrapper')
+);
+
 adminlinks
   .live('click', function(event) {
     if(!isTouch()) {
@@ -12,8 +17,6 @@ adminlinks
     event.preventDefault();
   })
   .live('tap', function(){ 
-    console.log("Tap");
-    adminlinks.die('tap');
     history.pushState({ path: this.path }, '', this.href);
     $(this).loadNextPage();
   })
@@ -21,70 +24,27 @@ adminlinks
       //console.log("Swipe");
   });
 
-$(window).bind('popstate', function() {
- loadPrevPage(location.pathname);
-})
+  $(window).bind('popstate', function() {
+  slider.prev();
+  })
 
 }
-
-
-
-function loadPrevPage(url){
-    var page = url + '/ #page';
-    var prevpage = createDiv('prevpage','page empty slide');
-       console.log("Loading " + page);
-   window.scroll(0, 0);
-   $('body').addClass('ui-loading');
- $('#prevpage').load(page, function() { 
-     console.log("Loaded " + page); 
-      $('#prevpage').addClass('out reverse');
-      $('#page').addClass('in reverse');
-      $('body').removeClass('ui-loading'); 
-      $('#prevpage').cleanupclasses();//Tidy up time
-      $('#nextpage').remove();
-      $('#page').attr('id', 'nextpage');
-      $('#prevpage').attr('id', 'page');
-      ready();
-  });
-
-}  
 
 Zepto.fn.loadNextPage = function() {
-    var o = $(this[0]) // It's your element
-    var url = o.attr('href');
-    var page = url + '/ #page';
-   var nextpage = createDiv('nextpage','page empty slide');
-   $('#nextpage').remove();
-   $('#page').after(nextpage);
-   var $nextpage = $('#nextpage');
-    console.log("Loading " + page);
-    window.scroll(0, 0); 
-    $('body').addClass('ui-loading');
-    
-    $nextpage.load(page, function() {
-      console.log("Loaded " + page);
-      $('#page').addClass('out');
-      $nextpage.addClass('in');
-      $('body').removeClass('ui-loading'); 
-       $nextpage.cleanupclasses();//Tidy up time
-       $('#prevpage').remove();
-       $nextpage.attr('id', 'page');
-       $('#page').attr('id', 'prevpage');
-       ready();
-     }); 
+  var o = $(this[0]) // It's your element
+  var url = o.attr('href');
+      console.log('Loading ' + url);
+  var nextpage = $('.slider li.current + li');
+  nextpage.load(url, function() {
+    console.log('Loaded ' + url);
+      slider.next();
+      var pos = slider.getPost();
+      $('.slider li').removeClass('current');
+      $('.slider li:eq(' + pos + ')').addClass('current');
+  })
+
 };
 
-function createDiv(id,cssclass) {
-  var div = document.createElement('div');
-  div.setAttribute('id', id); 
-  div.setAttribute('class', cssclass); 
-  return div;
-}
-
-Zepto.fn.cleanupclasses = function() {
-   var o = $(this[0]);
-   o.removeClass('empty');
-}
 
 function isTouch() {
   return ('ontouchstart' in window)
